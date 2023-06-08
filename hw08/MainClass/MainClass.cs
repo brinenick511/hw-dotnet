be258dfa541a4851cb5940f7f6a13cf3a8c33c98
brinenick511 <101073680+brinenick511@users.noreply.github.com>
2023-06-09 03:28:56 +0800

    Add files via upload

---
+++ hw08/MainClass/MainClass.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp
+{
+    public class Order
+    {
+        public string OrderId { get; set; }
+        public string Customer { get; set; }
+        public List<OrderDetail> Details { get; set; }
+        public Order()
+        {
+            OrderId = Guid.NewGuid().ToString();
+            Details = new List<OrderDetail>();
+        }
+        public Order(string customer, List<OrderDetail> details) : this()
+        {
+            OrderId = Guid.NewGuid().ToString();
+            Customer = customer;
+            Details = details;
+        }
+        public override string ToString()
+        {
+            string ans = $"Id:{OrderId}, customer:{Customer}";
+            //StringBuilder strBuilder = new StringBuilder();
+            //strBuilder.Append($"Id:{OrderId}, customer:{Customer}");
+            Details.ForEach(od => ans += ("\n\t" + od.ToString()));
+            return ans;
+        }
+    }
+    public class OrderDetail
+    {
+        public string Id { get; set; }
+        //public int Index { get; set; }
+        public string Good { get; set; }
+        public double Price { get; set; }
+        public string OrderId { get; set; }
+        public Order Order { get; set; }
+        public OrderDetail()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+        public OrderDetail(string good, double price)
+        {
+            Id = Guid.NewGuid().ToString();
+            //Index = index;
+            Good = good;
+            Price = price;
+        }
+        public override string ToString()
+        {
+            return $"[Id.:{Id},goods:{Good},price:{Price}]";
+        }
+    }
+}
---
+++ hw08/MainClass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var ods = new List<OrderDetail>();
+            ods.Add(new OrderDetail( "aaa", 1.1));
+            ods.Add(new OrderDetail( "aaa", 2.2));
+            ods.Add(new OrderDetail( "aaa", 3.3));
+            Order o = new Order("me", ods);
+
+            OrderService os = new OrderService();
+            os.RemoveAll();
+            os.AddOrder(o);
+            List<Order> orders = os.Orders;
+            orders.ForEach(oo => Console.WriteLine(oo));
+
+            Console.ReadKey();
+        }
+    }
+}
